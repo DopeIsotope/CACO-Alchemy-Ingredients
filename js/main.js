ba3aaf443886eae29428f653c25ce29c8edd2084
DopeIsotope <91102410+DopeIsotope@users.noreply.github.com>
2026-05-01 05:53:11 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -45,8 +45,9 @@ requestJsonData(ingredientDataPath, function(response) {
     document.getElementById("ingredient-effects-dropdown-03"),
     document.getElementById("ingredient-effects-dropdown-04")];
   let modsrcToggles = [
-    document.getElementById("show-bruma-ingredients"),
-    document.getElementById("show-hunterborn-ingredients")];
+  document.getElementById("show-bruma-ingredients"),
+  document.getElementById("show-hunterborn-ingredients"),
+  document.getElementById("show-rare-curios-ingredients")];
   let resultsParent = document.getElementById("ingredient-search-results");
   
   runIngredientDataBookkeeping(ingredientData, debugJson);
